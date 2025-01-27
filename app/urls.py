08e8cad294b92242522e83955af007496cbc68da
Felipe Rodrigues <felipe.git@outlook.com>
2025-01-27 02:55:56 -0300

    Created PUT request method

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,5 @@ from genres.views import genre_create_list_view, genre_detail_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('genres/', genre_create_list_view, name='genre-create-list'),
-    path('genre/<int:pk>/', genre_detail_view, name='genre-detail-view')
+    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view')
 ]
