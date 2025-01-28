c92fbc469ba23ab7896cbbd71243bd662a462832
Felipe Rodrigues <felipe.git@outlook.com>
2025-01-28 18:28:37 -0300

    Created actors CRUD

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
+from actors.views import ActorListCreateView, ActorRetrieveUpdateDestroyView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
-    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view')
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
+    
+    path('actors/', ActorListCreateView.as_view(), name='actor-create-list'),
+    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view')
 ]
