901e1b4ca12fa74b2a88070e0f882ef6dc4013a8
Felipe Rodrigues <felipe.git@outlook.com>
2025-01-29 19:39:04 -0300

    Fixing review url

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('reviews/', views.ReviewListCreateView.as_view(), name='review-list-create-view'),
     path('reviews/<int:pk>/', views.ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view')
