088fa5dc83f058703f2aa2b47bab0ca1712794dd
Felipe Rodrigues <felipe.git@outlook.com>
2025-01-30 15:23:10 -0300

    Adding serializers validators

--- movies/serializers.py
+++ movies/serializers.py
@@ -6,3 +6,13 @@ class MovieModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+    
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError('Movie release date cannot be before 1990.')
+        return value
+    
+    def validate_synopsis(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError('Movie synopsis cannot be longer than 200 characters.')
+        return value
