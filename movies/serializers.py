e1fe48608386fad40a35b8646127e3ce90e43469
Felipe Rodrigues <felipe.git@outlook.com>
2025-01-30 17:40:05 -0300

    Adding Serializer Method Field

--- movies/serializers.py
+++ movies/serializers.py
@@ -3,16 +3,25 @@ from movies.models import Movie
 
 
 class MovieModelSerializer(serializers.ModelSerializer):
+    rating = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
     
-    def validate_release_date(self, value):
-        if value.year < 1990:
-            raise serializers.ValidationError('Movie release date cannot be before 1990.')
-        return value
+    def get_rating(self, obj):
+        reviews = obj.reviews.all()
+
+        if reviews:
+            sum_reviews = 0
+            for review in reviews:
+                sum_reviews += review.rating
+            reviews_count = reviews.count()
+            return round(sum_reviews / reviews_count, 1)
+        
+        return None
     
     def validate_synopsis(self, value):
-        if len(value) > 200:
-            raise serializers.ValidationError('Movie synopsis cannot be longer than 200 characters.')
+        if len(value) > 1000:
+            raise serializers.ValidationError('Movie synopsis cannot be longer than 1000 characters.')
         return value
