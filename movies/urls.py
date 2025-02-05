1d17295b2d46eb5e1e1b43bf76ae958b17fe4e14
Felipe Rodrigues <felipe.git@outlook.com>
2025-02-05 17:04:15 -0300

    Added MovieStatsView and url

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('movies/', views.MovieListCreateView.as_view(), name='movie-list-create-view'),
-    path('movies/<int:pk>/', views.MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view')
+    path('movies/<int:pk>/', views.MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
+    path('movies/stats/', views.MovieStatsView.as_view(), name='movie-stats-view'),
 ]
--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('reviews/', views.ReviewListCreateView.as_view(), name='review-list-create-view'),
     path('reviews/<int:pk>/', views.ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view')
