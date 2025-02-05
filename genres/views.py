2a1f8d7f9d0bfccd7de0a1925f413b16e77b5062
Felipe Rodrigues <felipe.git@outlook.com>
2025-02-04 22:31:23 -0300

    Created GlobalDefaultPermission and added to actors, genres, movies and reviews views

--- genres/views.py
+++ genres/views.py
@@ -1,17 +1,17 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from app.permissions import GlobalDefaultPermission
 from genres.models import Genre
 from genres.serializers import GenreModelSerializer
-from genres.permissions import GenrePermissionClass
 
 
 class GenreListCreateView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated, GenrePermissionClass,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreModelSerializer
 
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated, GenrePermissionClass,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreModelSerializer
