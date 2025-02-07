3e133c1f06132516ef60393529524a5f86277f63
Felipe Rodrigues <felipe.git@outlook.com>
2025-02-06 22:46:50 -0300

    Fixed MovieListDetailSerializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,20 +1,15 @@
 from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
+from genres.serializers import GenreModelSerializer
+from actors.serializers import ActorModelSerializer
 
 
 class MovieModelSerializer(serializers.ModelSerializer):
-    rating = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
-
-    def get_rating(self, obj):
-        rating = obj.reviews.aggregate(Avg('rating'))['rating__avg']
-        if rating:
-            return round(rating, 2)
-        return None
 
     def validate_synopsis(self, value):
         if len(value) > 1000:
@@ -23,6 +18,16 @@ class MovieModelSerializer(serializers.ModelSerializer):
 
 
 class MovieListDetailSerializer(serializers.ModelSerializer):
+    actors = ActorModelSerializer(many=True)
+    genre = GenreModelSerializer()
+    rating = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = ['id', 'title', 'genre', 'actors', 'release_date', 'rating', 'synopsis']
+
+    def get_rating(self, obj):
+        rating = obj.reviews.aggregate(Avg('rating'))['rating__avg']
+        if rating:
+            return round(rating, 1)
+        return None
